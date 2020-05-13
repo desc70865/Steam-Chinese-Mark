5f57ff09a9a9d2f4ad931e9f97b32df6289c9b22
desc_inno <17777904+desc70865@users.noreply.github.com>
2020-05-13 11:58:43 +0800

    Update schinese_get.py

--- schinese_get.py
+++ schinese_get.py
@@ -1,17 +1,18 @@
 import re
 import requests
 
-url_head = "https://store.steampowered.com/search/?sort_by=&sort_order=0&supportedlang=schinese&page="
+url_head = "https://store.steampowered.com/search/?supportedlang=schinese%2Ctchinese&page="
 
-cookies = {'birthtime': '283993201', 'mature_content': '1'} # ~
+cookies = {'birthtime': '283993201', 'mature_content': '1'}
 res_appid = r"(?<=app\/)(\d{3,7})"
 
-total = 638  # 638 is magic num
+total = 719  # 719
 index = 1
-with open("appid_output.txt", "w") as fp:
+with open("chinese.json.txt", "w") as fp:
     while index <= total:
         page = requests.get(url_head + str(index), cookies=cookies).text
         links = re.findall(res_appid, page, re.I | re.S | re.M)
         fp.write(str(links) + '\n')
+        print(str(index) + ' / ' + str(total) + '|' + str(round(index / total * 100, 2)) + '%')
         index += 1
 fp.close()
