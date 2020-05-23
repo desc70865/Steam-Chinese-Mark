0e4de036ded8dea4cdf6a361dd3943e516ae45db
desc_inno <descinno@gmail.com>
2020-05-23 17:52:12 +0800

    Update get_chinese.py

--- get_chinese.py
+++ get_chinese.py
@@ -1,18 +1,29 @@
 import re
 import requests
 
+# this .py can used to extract all appids from https://store.steampowered.com/search/?* to create a json file
+# json format: {"353370": "Chinese",...}
+
+# lock the search page and calc how many pages you need
+# use regexp get appids
+# format into json
+
 url_head = "https://store.steampowered.com/search/?supportedlang=schinese%2Ctchinese&page="
+res_appid = r"(?<=app\/)(\d+)|(?<=sub\/)(\d+)|(?<=bundle\/)(\d+)"
 
-cookies = {'birthtime': '283993201', 'mature_content': '1'}
-res_appid = r"(?<=app\/)(\d{3,7})"
-
-total = 719  # 719
+total = 3  # magic num by totalAll / 25
 index = 1
+
 with open("chinese.json.txt", "w") as fp:
     while index <= total:
-        page = requests.get(url_head + str(index), cookies=cookies).text
+        page = requests.get(url_head + str(index)).text
         links = re.findall(res_appid, page, re.I | re.S | re.M)
-        fp.write(str(links) + '\n')
-        print(str(index) + ' / ' + str(total) + '|' + str(round(index / total * 100, 2)) + '%')
+        for i in range(len(links)):
+            links[i] = '"' + links[i] + '": "Chinese"'
+        fp.write(str(links))
+        print(str(index) + '/' + str(total) + ' | ' + str(round(index / total * 100, 2)) + '%')
         index += 1
 fp.close()
+
+# then remove all ' and change []
+# notice that different brackets use means very different list style in python
